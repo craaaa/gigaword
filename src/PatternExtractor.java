81e6f83cd7903e616b1f23b8714a9f1c9d768fb7
Cara Leong <heycraa@gmail.com>
2018-02-18 16:24:39 +0800

    Finish parser

--- src/PatternExtractor.java
+++ src/PatternExtractor.java
@@ -1,29 +1,87 @@
-import edu.stanford.nlp.parser.lexparser.EnglishTreebankParserParams;
-import edu.stanford.nlp.parser.lexparser.TreebankLangParserParams;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphFactory;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.semgraph.semgrex.SemgrexMatcher;
 import edu.stanford.nlp.semgraph.semgrex.SemgrexPattern;
-import edu.stanford.nlp.trees.GrammaticalStructure;
-import edu.stanford.nlp.trees.GrammaticalStructureFactory;
-import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.logging.Redwood;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Properties;
 
 public class PatternExtractor {
-    SemgrexPattern semgrex;
-    GrammaticalStructureFactory factory;
+    /**
+     * Takes in a directory and returns an iterator of Strings
+     * over all the files in the directory
+     */
 
-    public PatternExtractor(String pattern){
-        semgrex = SemgrexPattern.compile(pattern);
-        TreebankLangParserParams params = new EnglishTreebankParserParams();
-        factory = params.treebankLanguagePack().grammaticalStructureFactory(
-                params.treebankLanguagePack().punctuationWordRejectFilter(),
-                params.typedDependencyHeadFinder());
+    private StanfordCoreNLP pipeline;
+    private ObjectOutputStream verbSavedSentences;
+    private ObjectOutputStream nounSavedSentences;
+    private static Redwood.RedwoodChannels log = Redwood.channels(GigawordParser.class);
+
+
+    public GigawordParser(Path saveFileDir, String fileName) throws IOException {
+        Properties props = new Properties();
+        verbSavedSentences = new ObjectOutputStream(
+                new FileOutputStream( saveFileDir.resolve(fileName + "v").toFile()));
+        nounSavedSentences = new ObjectOutputStream(
+                new FileOutputStream( saveFileDir.resolve(fileName + "n").toFile()));
+        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse");
+        pipeline = new StanfordCoreNLP(props);
     }
 
-    public SemgrexMatcher matches(Tree tree) {
-        GrammaticalStructure structure = factory.newGrammaticalStructure(tree);
-        SemanticGraph graph = SemanticGraphFactory.generateEnhancedDependencies(structure);
-        SemgrexMatcher matcher = semgrex.matcher(graph);
-        return matcher;
+
+    public void pipe(String text){
+        Annotation document = new Annotation(text);
+        pipeline.annotate(document);
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+        for(CoreMap sentence: sentences) {
+            log.info("Checking if sentence matches:" + sentence);
+            SemanticGraph g = sentence.get(
+                    SemanticGraphCoreAnnotations.EnhancedPlusPlusDependenciesAnnotation.class);
+            log.info("Generated semantic graph for sentence: " + sentence);
+
+            // System.out.println("Relations:" + g.findAllRelns(UniversalEnglishGrammaticalRelations.getNmod("for")));
+
+            // Save verb patterns
+            SemgrexPattern verbPattern = SemgrexPattern.compile("{tag:/VB.*/}=verb >/nmod:for/=rel {}=duration");
+            saveGraphIfMatches(verbSavedSentences, verbPattern, g);
+
+            // Save noun patterns
+            SemgrexPattern nounPattern = SemgrexPattern.compile("{tag:/NN.*/}=verb >/nmod:for/=rel {}=duration");
+            saveGraphIfMatches(nounSavedSentences, nounPattern, g);
+        }
+    }
+
+    private static void saveGraphIfMatches(ObjectOutputStream savedSentences,
+                                           SemgrexPattern pattern, SemanticGraph g) {
+        if (matchesSemgrex(pattern, g)){
+            try {
+                savedSentences.writeObject(g);
+                log.info("Saved sentence: " + g.toRecoveredSentenceString());
+            } catch (IOException e) {
+                log.err("Failed to save sentence: " + g.toRecoveredSentenceString());
+            }
+        }
+    }
+
+    private static void printMatches(SemgrexMatcher matcher) {
+        while (matcher.find()) {
+            System.out.println(matcher.getNode("verb") + " " +
+                    matcher.getRelnString("rel") + " " +
+                    matcher.getNode("duration"));
+        }
+    }
+
+    private static boolean matchesSemgrex(SemgrexPattern pattern, SemanticGraph graph){
+        SemgrexMatcher matcher = pattern.matcher(graph);
+        return matcher.find();
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,49 @@
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class Runner {
 
+    private static final String GIGAWORD_DIR = "/home/craa/Documents/gigaword";
+    private static final Path SAVE_DIR =  Paths.get("/home/craa/Documents/gigaword/save");
+    private static final String[] PAPER_NAMES = {"afe", "apw", "nyt", "xie"};
+
+    public static void parseFile(Path file) {
+        List<String> parsedText = parseSgmlFromFile(file);
+
+        PatternExtractor extractor;
+        try {
+            extractor = new PatternExtractor(SAVE_DIR, file.getFileName().toString());
+        } catch (IOException e) {
+            return;
+        }
+        for (String text : parsedText) {
+            extractor.pipe(text);
+        }
+    }
+
+    private static List<String> parseSgmlFromFile(Path file) {
+
+    }
+
     public static void main(String[] args) {
-        String text = "An 18-year-old student, Abdel Aziz Hussein Abdelbaki, " +
-                "was jailed for 15 years.";
-        PatternExtractor extractor = new PatternExtractor("{pos:/VB.*/=verb >/NMOD:FOR/ {NER:DURATION}=duration");
+
+        Arrays.stream(PAPER_NAMES)
+                .map(p -> Paths.get(GIGAWORD_DIR, p))
+                .flatMap(path -> {
+                    try {
+                        return Files.list(path);
+                    } catch (IOException e) {
+                        System.err.println("Couldn't list files in " + path.toString());
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .parallel()
+                .forEach(Runner::parseFile);
     }
 }
